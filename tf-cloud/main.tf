fdbb0139bead75423da569c9e4859f079d992130
aashish <apple@Apples-MacBook-Pro.local>
2023-09-07 21:39:27 +0530

    Added tf cloud values

---
+++ tf-cloud/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  ami = "ami-051f7e7f6c2f40dc1"
+}
+
+variable "list" {
+  type    = list(any)
+  default = ["t2.micro", "t2.small"]
+}
+resource "aws_instance" "ec2" {
+  ami           = local.ami
+  instance_type = var.list[0]
+
+}
