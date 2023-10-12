54c0fad8c499be58e45cd773f395c7226834555b
Aashish Chetwani <aashishchetwani@Aashishs-MacBook-Pro.local>
2023-10-12 15:11:00 +0530

    Runnning main.tf in terraform cloud

--- main.tf
+++ main.tf
@@ -1,26 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-}
-
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
 }
 
-locals {
-  ami = "ami-067d1e60475437da2"
-}
-
-variable "list" {
-  type    = list(any)
-  default = ["t2.micro", "t2.small"]
-}
-resource "aws_instance" "ec2" {
-  ami           = local.ami
-  instance_type = var.list[0]
-
+resource "aws_instance" "myec2" {
+   ami = "ami-067d1e60475437da2"
+   instance_type = "t2.micro"
 }
